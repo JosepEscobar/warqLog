c4900c00976743f08537448c2bc826409e403244
Josep Escobar <josep.escobar@hpcds.com>
2019-10-24 16:56:42 +0200

    Initial Commit

---
+++ warqLog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  warqLog
+//
+//  Created by Josep Escobar on 24/10/2019.
+//  Copyright © 2019 Josep Escobar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
